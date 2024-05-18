c9b8e21b87298f567730fa733762f02cb2a751a4
정영일 <bbangwon@outlook.com>
2024-05-18 20:26:01 +0900

    숙박시설 예제

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,23 @@ import './App.css'
 // import Heading from './Heading';
 // import ColorList from './ColorList';
 // import Slots from './Slots';
-import ShoppingList from './ShoppingList';
+// import ShoppingList from './ShoppingList';
+import PropertyList from './PropertyList';
 
-const data = [
-  {id: 1, item: 'eggs', quantity: 12, completed: false},
-  {id: 2, item: 'milk', quantity: 1, completed: true},
-  {id: 3, item: 'chicken breasts', quantity: 4, completed: false},
-  {id: 4, item: 'carrots', quantity: 12, completed: true}
+// const data = [
+//   {id: 1, item: 'eggs', quantity: 12, completed: false},
+//   {id: 2, item: 'milk', quantity: 1, completed: true},
+//   {id: 3, item: 'chicken breasts', quantity: 4, completed: false},
+//   {id: 4, item: 'carrots', quantity: 12, completed: true}
+// ]
+
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
 ]
 
 function App() {
@@ -38,7 +48,8 @@ function App() {
 
     {/* <Slots s1="🍉" s2="🍉" s3="🍇" />
     <Slots s1="🍉" s2="🍉" s3="🍉" /> */}
-    <ShoppingList items={data} />
+    {/* <ShoppingList items={data} /> */}
+    <PropertyList properties={properties} />
   </div>
   );
 }
