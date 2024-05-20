719a2e2bf08685d842f3467068f3e39c27e5fd11
youngil <youngil@placeb.com>
2024-05-20 20:02:07 +0900

    useState()와 렌더링

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 
 export default function Counter() {    
     const [num, setNum] = useState(5); //useState 훅 사용
-    const changeNum = () => setNum(num + 1); //setNum을 사용하여 num을 증가시킴
+    console.log("COMPONENT HAS BEEN EXECTUED AGAIN!");
+    console.log(`num is ${num}`);
+
+    const changeNum = () =>{ 
+        setNum(num + 1); //setNum을 사용하여 num을 증가시킴
+        console.log("SET NUM HAS RUN!");
+        console.log(`num is now ${num}`);
+    }
     return (
         <div>
             <p>The count is: {num} </p>
